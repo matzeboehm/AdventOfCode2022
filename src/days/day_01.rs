cb5c991392e3f8111e96454d56bdf64d28917cd1
Matthias Boehm <matthias1.boehm@web.de>
2023-05-20 21:40:38 +0100

    task 2

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -2,30 +2,30 @@ use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
 pub fn task_1(){
-    let file = File::open("src/days/input/day_01.txt").unwrap();
+    let file: File = File::open("src/days/input/day_01.txt").unwrap();
     let lines = BufReader::new(file).lines();
 
 
-    let mut temp: i32 = 0;
-    let mut output: i32 = 0;
-    let mut max_user: i32 = 0;
-    let mut current_elf: i32 = 1;
+    let mut elf_calories: i32 = 0;
+    let mut vec: Vec<i32> = Vec::new();
     for line in lines {
         let value: String = line.unwrap();
         
         if value.is_empty() {
-            if temp > output{
-                output = temp;
-                max_user = current_elf;
-            }
-            temp = 0;
-            current_elf += 1;
+            vec.push(elf_calories);
+            elf_calories = 0;
         } else {
             let converted_value: i32 =  value.parse().unwrap();
-            temp += converted_value;
+            elf_calories += converted_value;
         }
     }
 
-    println!("Output {}, Max User {}", output, max_user);
+    vec.sort();
+
+    let last_three: &[i32] = &vec[vec.len() - 3 .. vec.len()];
+    println!("{:?} ", last_three);
+
+    let sum_last_three: i32 = last_three.iter().sum();
+    println!("{sum_last_three}")
 }
     
